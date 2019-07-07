546b1ef41671655344fc78f2dcdb74f53cd8081d
Vinccool96 <Vinccool96@gmail.com>
2019-07-07 11:48:16 -0400

    Listeners

--- src/main/java/controleur/Controleur.java
+++ src/main/java/controleur/Controleur.java
@@ -1,5 +1,9 @@
 package controleur;
 
+import com.github.strikerx3.jxinput.XInputAxes;
+import com.github.strikerx3.jxinput.XInputComponents;
+import com.github.strikerx3.jxinput.XInputDevice;
+import com.github.strikerx3.jxinput.exceptions.XInputNotLoadedException;
 import javafx.fxml.FXML;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -16,7 +20,38 @@ public class Controleur {
 
     @FXML
     public void initialize() {
+        // Retrieve all devices
+        XInputDevice[] devices = new XInputDevice[0];
+        try {
+            devices = XInputDevice.getAllDevices();
+        } catch (XInputNotLoadedException e) {
+            e.printStackTrace();
+        }
 
+        // Retrieve the device for player 1
+        XInputDevice device = devices[0]; // or XInputDevice.getDeviceFor(0)
+
+        launch(device);
+
+    }
+
+    private void launch(XInputDevice device) {
+        Runnable r = () -> {
+            while (device.poll()) {
+                XInputComponents components = device.getComponents();
+                XInputAxes axes = components.getAxes();
+                float rightAxisXDelta = axes.ry;
+                float leftAxisXDelta = axes.ly;
+                System.out.println("rightAxisXDelta: " + rightAxisXDelta);
+                System.out.println("leftAxisXDelta: " + leftAxisXDelta);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        new Thread(r).start();
     }
 
     @FXML
