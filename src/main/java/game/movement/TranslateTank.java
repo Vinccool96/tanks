c23df3f853b0819b426a02b4c23ee8e59f5f1301
Vinccool96 <Vinccool96@gmail.com>
2019-07-11 14:41:47 -0400

    Translation

--- src/main/java/game/movement/TranslateTank.java
+++ src/main/java/game/movement/TranslateTank.java
@@ -9,8 +9,8 @@ public class TranslateTank {
     public static void translate(Tank tank) {
         double speed = tank.getLeftSpeed();
         double distanceTraveled = speed * MoveTank.TIME_OF_STEP;
-        PhysicsVector vector = new PhysicsVector();
         double orientation = speed < 0 ? -tank.getOrientation() : tank.getOrientation();
+        PhysicsVector vector = new PhysicsVector();
         vector.setVectorMagnitude(orientation, distanceTraveled);
         tank.setPosX(tank.getPosX() + vector.getX());
         tank.setPosY(tank.getPosY() + vector.getY());
